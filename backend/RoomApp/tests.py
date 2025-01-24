e938f84e65886c061e1057a513cb6f085c888b12
Fodor Robert <139761086+FodorRobert-Stefan@users.noreply.github.com>
2025-01-24 23:20:23 +0200

    15.Testing the creation of user

--- backend/RoomApp/tests.py
+++ backend/RoomApp/tests.py
@@ -2,15 +2,15 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from rest_framework import status
 from RoomApp.models import Room
-
+from django.contrib.auth.models import User
 class RegisterRoomViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.url = reverse('register_room') 
-
+        self.register_room_url = reverse('register_room') 
+        self.register_user_url = reverse('register_user')
     def test_successful_room_registration(self):
-        response = self.client.post(self.url, {
-            'floor': 1,
+        response = self.client.post(self.register_room_url, {
+            'floor': 1, 
             'type': 'suite'
         }, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -21,6 +21,16 @@ class RegisterRoomViewTest(TestCase):
         self.assertEqual(room.floor, 1)
         self.assertEqual(room.type, 'suite')
         self.assertTrue(room.available)
+    def test_successful_user_registration(self):
+        response = self.client.post(self.register_user_url,{
+            'email':"test@yaghoo.com",
+            'first_name':'test_first_mna,e',
+            'last_name':'test_l;ast_name',
+            'password':'test_password'
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
 
     def tearDown(self):
         Room.objects.all().delete()
+        User.objects.all().delete()
